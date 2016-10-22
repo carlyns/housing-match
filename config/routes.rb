388d72513bf9fbb2d9bfd7d5c7d5216e42f44db0
mmazanec22 <melanie.mazanec@gmail.com>
2016-10-22 11:45:31 -0500

    Set up register, login, sign out with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  root 'application#index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "signup" => 'users/registrations#new'
+    get "login" => 'users/sessions#new'
+    get "logout" => 'users/sessions#destroy'
+  end
+
 end
